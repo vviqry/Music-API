cdb6f9555813c0181a4b2a64da3d00ff652215b7
Fikri <fikrymutsaqoful@gmail.com>
2026-03-22 13:41:57 +0700

    chore: add start script and dynamic PORT for deployment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const multer = require("multer");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // ============================================================
 // DATABASE SEMENTARA (In-Memory Array)
